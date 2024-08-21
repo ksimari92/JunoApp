8a1a6cd2d863483c8ea4fd72bfef9aef0f026d93
Karen Simari <karensimari@live.com.ar>
2024-08-20 22:01:15 -0300

    cambio img

--- src/components/Header/CarritoIcon.jsx
+++ src/components/Header/CarritoIcon.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import {Link} from 'react-router-dom'
 import { JunoContext } from '../../context/JunoContext'
+import carritoIMG from "../../assets/img/cesta-de-la-compra.png";
 
 const CarritoIcon = () => {
 
@@ -9,7 +10,7 @@ const CarritoIcon = () => {
   return (
     <Link to={'/carrito'} style={{textDecoration:'none'}}>
     <div style={{marginRight:'10px'}}>
-        <img src="/src/assets/img/cesta-de-la-compra.png" alt="Cart" style={{width: '2em'}} /><span  style={{fontSize: '1em', padding: '5px', marginLeft:'-10px', backgroundColor:'black', color: 'white', borderRadius:'40%'}}>{cantidadTotal}</span>
+        <img src={carritoIMG} alt="Cart" style={{width: '2em'}} /><span  style={{fontSize: '1em', padding: '5px', marginLeft:'-10px', backgroundColor:'black', color: 'white', borderRadius:'40%'}}>{cantidadTotal}</span>
     </div>
     </Link>
   )
--- src/components/Home/Galeria.jsx
+++ src/components/Home/Galeria.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Home.css'
+import img1 from "../../assets/img/airForce.jpeg"
+import img2 from "../../assets/img/vans.jpeg"
+import img3 from "../../assets/img/campus.jpg"
 
 const Galeria = () => {
 
@@ -8,9 +11,9 @@ const Galeria = () => {
     <>
     <h3 style={{textAlign: 'center'}}>Productos destacados</h3>
     <div className='container-galeria'>
-        <img src="/src/assets/img/airForce.jpeg" alt="Nike" />
-        <img src="/src/assets/img/vans.jpeg" alt="vans" />
-        <img src="/src/assets/img/campus.jpg" alt="campus" />
+        <img src={img1} alt="Nike" />
+        <img src={img2} alt="vans" />
+        <img src={img3} alt="campus" />
     </div>
     </>
   )
